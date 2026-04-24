33a6cdd3b89fd2c9db723b1e0e996384ac7621ab
Sireyvorth <lunsereyvorth@gmail.com>
2026-04-25 01:19:02 +0700

    feat: add useUploadProductImage hook for handling product image uploads with React Query

--- src/hooks/product/useUploadProductImage.ts
+++ src/hooks/product/useUploadProductImage.ts
@@ -2,18 +2,18 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { uploadProductImage } from "@/services/product.service";
 import { toast } from "sonner";
 
-export const useUploadProductImage = () =>{
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn:({id,request}:{id:number, request:File})=> {
-            return uploadProductImage(id,request)
-        },
-        onSuccess:()=>{
-            toast.success("Product Image Upload Successfully..!");
-            queryClient.invalidateQueries({queryKey:["products"]})
-        },
-        onError:()=>{
-            toast.error("Product Image Upload Failed..!")
-        }
-     })
-}
+export const useUploadProductImage = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ imageId, request }: { imageId: number; request: File }) => {
+      return uploadProductImage(imageId, request);
+    },
+    onSuccess: () => {
+      toast.success("Hook: Product Image Upload Successfully..!");
+      queryClient.invalidateQueries({ queryKey: ["products"] });
+    },
+    onError: () => {
+      toast.error("Product Image Upload Failed..!");
+    },
+  });
+};
